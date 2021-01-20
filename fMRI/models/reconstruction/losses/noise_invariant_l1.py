26b2f413b86d98f7d635f0c73c3178e1ccf2f276
Jon Andre Ottesen <joniandre1497@hotmail.com>
2021-01-20 10:33:06 +0100

    add noise invariant l1 for testing

---
+++ fMRI/models/reconstruction/losses/noise_invariant_l1.py
@@ -0,0 +1,46 @@
+from typing import Union
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class NoiseInvariantL1(nn.Module):
+    """
+    Just testing out some noise invarience for L1 loss
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.l1 = torch.nn.L1Loss(reduction='none')
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    def forward(self,
+                X: torch.Tensor,
+                Y: torch.Tensor,
+                sigma: float = None,
+                ):
+        """
+        Args:
+            X (torch.Tensor): Prediction
+            Y (torch.Tensor): Ground truth
+            data_range (float): Difference between maximum and minimum value
+        """
+        if sigma is None:
+            sigma = torch.std(Y[:, :50, :50]).cpu().item()
+
+        l1 = self.l1(X, Y)
+        factor = 1 - torch.exp(-(l1 - sigma)**2/(2*sigma**2))
+        loss = l1*factor
+
+        return torch.mean(loss)
+
+if __name__=='__main__':
+    a = torch.rand((3, 100, 100))
+    b = torch.rand((3, 100, 100)) + 2
+
+    loss = NoiseInvariantL1()
+    std = a.std()
+    loss(a, b, sigma=None)
