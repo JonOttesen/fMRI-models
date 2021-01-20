7ccf0cf33a93588d4df848143fdc050be2d06282
Jon Andre Ottesen <joniandre1497@hotmail.com>
2021-01-20 10:48:19 +0100

    test loss func

--- fMRI/models/reconstruction/losses/noise_invariant_l1.py
+++ fMRI/models/reconstruction/losses/noise_invariant_l1.py
@@ -28,8 +28,7 @@ class NoiseInvariantL1(nn.Module):
             data_range (float): Difference between maximum and minimum value
         """
         if sigma is None:
-            sigma = Y[:, :, :50, :50].cpu().detach().std(axis=(1, 2, 3))
-            print(sigma)
+            sigma = Y[:, :, :50, :50].cpu().detach().std(axis=(1, 2, 3), keepdim=True)
 
         l1 = self.l1(X, Y)
         factor = 1 - torch.exp(-(l1 - sigma)**2/(2*sigma**2))
@@ -38,8 +37,8 @@ class NoiseInvariantL1(nn.Module):
         return torch.mean(loss)
 
 if __name__=='__main__':
-    a = torch.rand((3, 100, 100))
-    b = torch.rand((3, 100, 100)) + 2
+    a = torch.rand((4, 3, 100, 100))
+    b = torch.rand((4, 3, 100, 100))
 
     loss = NoiseInvariantL1()
     std = a.std()
